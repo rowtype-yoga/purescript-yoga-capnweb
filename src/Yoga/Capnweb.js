397f88f4a7723d5579d2b0054a3e17322e81955f
Mark <mark.eibes@gmail.com>
2026-02-15 16:30:03 +0100

    Initial yoga-capnweb: client + server FFI

--- src/Yoga/Capnweb.js
+++ src/Yoga/Capnweb.js
@@ -2,7 +2,7 @@ import { newWebSocketRpcSession } from "capnweb";
 
 export const connectImpl = (url) => () => newWebSocketRpcSession(url);
 
-export const callImpl = (session, method, args) => session[method](...args);
+export const callImpl = (session, method, args) => Promise.resolve(session[method](...args));
 
 export const disposeImpl = (session) => () => {
   if (session[Symbol.dispose]) session[Symbol.dispose]();
