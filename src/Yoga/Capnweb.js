6bc67a9dd5c83808219f78ff94f3666e787ee38b
Mark <mark.eibes@gmail.com>
2026-02-15 16:35:11 +0100

    Fix RpcPromise -> Promise conversion

--- src/Yoga/Capnweb.js
+++ src/Yoga/Capnweb.js
@@ -1,8 +1,12 @@
 import { newWebSocketRpcSession } from "capnweb";
 
+const toPromise = (rpcPromise) => new Promise((resolve, reject) => {
+  rpcPromise.then(resolve, reject);
+});
+
 export const connectImpl = (url) => () => newWebSocketRpcSession(url);
 
-export const callImpl = (session, method, args) => Promise.resolve(session[method](...args));
+export const callImpl = (session, method, args) => toPromise(session[method](...args));
 
 export const disposeImpl = (session) => () => {
   if (session[Symbol.dispose]) session[Symbol.dispose]();
